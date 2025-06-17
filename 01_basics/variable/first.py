e32fbee6da710859a4c601c2c82039509c108359
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2025-06-17 05:27:30 +0000

    inital basic code practice

---
+++ 01_basics/variable/first.py
@@ -0,0 +1,16 @@
+# Variable Names
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+# Rules for Python variables:
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A vari# able name cannot be any of the Python keywords.
+
+name = "Sulagna Pradhan"
+college_name = "Asutosh college"
+age = 21
+
+print ("My name is "+ name + "\nI am a student of "+ college_name)
---
+++ 01_basics/variable/second.py
@@ -0,0 +1,13 @@
+# we can assign value to multiple variable in one line also 
+
+a, b, c = "apple", "banana", "guava"
+
+print (a, b, c)
+
+# we can assign same value to multiple variable in single line 
+
+x = y = z = "Dog"
+
+print(x)
+print(y)
+print(z)
